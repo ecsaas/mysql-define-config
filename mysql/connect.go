ccf525cac89f5eec65fca5967b70e4f28f394d17
manhbv.giaminh <manhbv.giaminh@gmail.com>
2022-10-26 09:41:23 +0700

    fix error

--- mysql/connect.go
+++ mysql/connect.go
@@ -4,23 +4,24 @@ import (
 	"database/sql"
 	"fmt"
 	"net/url"
-	"os"
 
-	"github.com/ecsaas/mysql-define-config/DEFINE_VARIABLES/MYSQL_ENV"
 	_ "github.com/go-sql-driver/mysql"
 )
 
-var SqlDb *sql.DB
-var SqlErr error
+type Connection struct {
+	MysqlUser     string
+	MysqlPassword string
+	MysqlIp       string
+	MysqlDbName   string
+}
 
-func SqlInit() (db *sql.DB) {
+func SqlInit(c Connection) (*sql.DB, error) {
 	var applyURI = fmt.Sprintf(
 		"%s:%s@tcp(%s)/%s",
-		url.QueryEscape(os.Getenv(MYSQL_ENV.MYSQL_USER)),
-		url.QueryEscape(os.Getenv(MYSQL_ENV.MYSQL_PASSWORD)),
-		os.Getenv(MYSQL_ENV.MYSQL_IP),
-		os.Getenv(MYSQL_ENV.MYSQL_DB_NAME),
+		url.QueryEscape(c.MysqlUser),
+		url.QueryEscape(c.MysqlPassword),
+		c.MysqlIp,
+		c.MysqlDbName,
 	)
-	SqlDb, SqlErr = sql.Open("mysql", applyURI)
-	return
+	return sql.Open("mysql", applyURI)
 }
--- mysql/readme.md
+++ mysql/readme.md
@@ -1,5 +1,5 @@
 # Config MySql 8.0
 
 ```shell
-
+ docker run --restart=always --name=mysql -d -v $PWD/data:/var/lib/mysql -p 3306:3306 mysql/mysql-server:8.0
 ```
